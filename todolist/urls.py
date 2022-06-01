d9f89a3bbe7a01386182486522beebeffe37c8ad
Tadaka Surya Teja <30947856+TadakaSuryaTeja@users.noreply.github.com>
2022-06-01 18:23:50 +0530

    Added gatewas (#2)
    Added gatewas (#2)

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+
+from todolist import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add', views.add_todo, name='add'),
-    path('completed/<todo_id>', views.completed_todo, name='completed'),
-    path('deletecomplete', views.delete_completed, name='deletecomplete'),
-    path('deleteall', views.delete_all, name='deleteall'),
-    ]
+    path("", views.index, name="index"),
+    path("add", views.add_todo, name="add"),
+    path("completed/<todo_id>", views.completed_todo, name="completed"),
+    path("deletecomplete", views.delete_completed, name="deletecomplete"),
+    path("deleteall", views.delete_all, name="deleteall"),
+]
--- todolist/views.py
+++ todolist/views.py
@@ -3,39 +3,58 @@ from django.views.decorators.http import require_POST
 
 from .models import Todolist
 from .forms import TodoListForm
+
 # Create your views here.
 
 
 def index(request):
-    todo_items = Todolist.objects.order_by('id')
+    """
+    Rendering Index page
+    """
+    todo_items = Todolist.objects.order_by("id")
     form = TodoListForm()
-    context = {'todo_items': todo_items, 'form': form}
-    return render(request, 'todolist/index.html', context)
+    context = {"todo_items": todo_items, "form": form}
+    return render(request, "todolist/index.html", context)
 
 
 @require_POST
 def add_todo(request):
+    """
+    add to do post request
+    """
     form = TodoListForm(request.POST)
     if form.is_valid():
-        new_todo = Todolist(text=request.POST['text'])
+        new_todo = Todolist(text=request.POST["text"])
         new_todo.save()
 
-    return redirect('index')
+    return redirect("index")
 
 
 def completed_todo(request, todo_id):
+    """
+    Completed to do function
+    returns: redirect to index page
+    """
     todo = Todolist.objects.get(pk=todo_id)
     todo.completed = True
     todo.save()
 
-    return redirect('index')
+    return redirect("index")
 
 
 def delete_completed(request):
+    """
+    deletes the completed task and redirects to index
+    returns: redirect to index home page
+    """
     Todolist.objects.filter(completed__exact=True).delete()
-    return redirect('index')
+    return redirect("index")
 
 
 def delete_all(request):
+    """
+    deletes all tasks and redirects to index
+    returns: redirects to index homepage
+    """
     Todolist.objects.all().delete()
-    return redirect('index')
+    return redirect("index")
